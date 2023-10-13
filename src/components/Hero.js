c6c7d2916c698d3b56aa318c6a7b595e657a5725
jasonLapina <lapina.jason@gmail.com>
2023-10-13 10:42:00 +0800

    hero style change

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -14,49 +14,62 @@ function Hero() {
 
   return (
     <Center pos='relative' w='100vw' h='100vh'>
-      <Box pos='absolute' top='120px' left='40px' fontSize='64px'>
-        <Text as='span'>{text}</Text>
+      <Box
+        pos='absolute'
+        bottom='25%'
+        left='50%'
+        fontSize='64px'
+        zIndex='2'
+        transform='translateX(-50%)'
+      >
+        <Text filter='drop-shadow(1px 1px 4px white)' as='span'>
+          {text}
+        </Text>
         <Cursor cursorBlinking cursorStyle='_' cursorColor='coral' />
         <Text opacity='0' pointerEvents='none' lineHeight='0'>
           Get to know me!
         </Text>
-        {showActions && (
-          <HStack
-            as={motion.div}
-            initial={{
-              opacity: 0,
-            }}
-            animate={{ opacity: 1 }}
-            gap='16px'
-            justify='center'
-          >
-            <Button
-              _hover={{
-                filter: "brightness(1.3)",
-              }}
-              boxShadow='2px 2px 4px gray'
-              color='white'
-              bgColor='coral'
-              transition='all .4s'
-            >
-              Contact
-            </Button>
-            <Button
-              _hover={{
-                filter: "brightness(1.3)",
-              }}
-              boxShadow='2px 2px 4px gray'
-              variant='outline'
-              borderColor='coral'
-              transition='all .4s'
-            >
-              Projects
-            </Button>
-          </HStack>
-        )}
       </Box>
+      {showActions && (
+        <HStack
+          pos='absolute'
+          bottom='25%'
+          transform='translate(-50%, 40px)'
+          left='50%'
+          as={motion.div}
+          initial={{
+            opacity: 0,
+          }}
+          animate={{ opacity: 1 }}
+          gap='16px'
+          justify='center'
+        >
+          <Button
+            _hover={{
+              filter: "brightness(1.3)",
+            }}
+            boxShadow='2px 2px 4px gray'
+            color='white'
+            bgColor='coral'
+            transition='all .4s'
+          >
+            Contact
+          </Button>
+          <Button
+            _hover={{
+              filter: "brightness(1.3)",
+            }}
+            boxShadow='2px 2px 4px gray'
+            variant='outline'
+            borderColor='coral'
+            transition='all .4s'
+          >
+            Projects
+          </Button>
+        </HStack>
+      )}
       <Text
-        textShadow='2px 2px 4px cyan'
+        filter='drop-shadow(2px 2px 4px cyan)'
         fontWeight='thin'
         pos='absolute'
         zIndex='-1'
@@ -64,13 +77,24 @@ function Hero() {
       >
         DEVELOPER
       </Text>
-      <Image
+      <Box
+        h='90%'
+        aspectRatio='1/1'
+        bgPos='center'
+        bgSize='cover'
+        bgRepeat='no-repeat'
+        bgImage={
+          "linear-gradient(to bottom, rgba(245,245,245,0), rgba(245,245,245,.4)), url('/assets/hero-me.png')"
+        }
+        borderRadius='full'
+      />
+      {/* <Image
         boxShadow='2px 2px 16px black'
         h='90%'
         borderRadius='full'
         pos='absolute'
         src='/assets/hero-me.png'
-      />
+      /> */}
     </Center>
   );
 }
