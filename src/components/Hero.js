1f3cce333b50bebd2ea3e6a19d864e170cfdecd2
jasonLapina <lapina.jason@gmail.com>
2023-10-27 02:10:50 +0800

    rework hero section desktop

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,27 +4,11 @@ import { useTypewriter, Cursor } from "react-simple-typewriter";
 import MotionBox from "./UI/MotionBox";
 
 function Hero() {
-  const [showActions, setShowActions] = useState(false);
-  const [showTypewriter, setShowTypewriter] = useState(false);
-
   const [text] = useTypewriter({
-    words: [
-      "           ",
-      "Hi, I'm Jason!",
-      "Web Developer",
-      "Get to know me!",
-    ],
-    loop: 1,
+    words: ["Designer.", "Developer."],
+    loop: 0,
     typeSpeed: 50,
-    onLoopDone: () => setShowActions(true),
   });
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowTypewriter(true);
-    }, 2900);
-    return () => clearTimeout(timer);
-  }, []);
 
   const handleClick = (section) => {
     document.getElementById(section).scrollIntoView({ behavior: "smooth" });
@@ -37,71 +21,61 @@ function Hero() {
       h={{ base: "70vh", md: "100vh" }}
       mb={{ base: "-120px" }}
     >
-      {showTypewriter && (
-        <Box
-          pos='absolute'
-          bottom={{ base: "28%", md: "25%" }}
-          left='50%'
-          fontSize={{ base: "24px", md: "48px" }}
-          zIndex={3}
-          transform='translateX(-50%)'
-        >
-          <Text filter='drop-shadow(1px 1px 4px white)' as='span'>
+      <MotionBox
+        initial={{
+          transform: "translate(-50%,100%)",
+          opacity: 0,
+        }}
+        animate={{
+          transform: "translateX(-50%)",
+          opacity: 1,
+        }}
+        transition={{ duration: 1, delay: 1 }}
+        pos='absolute'
+        bottom={{ base: "28%", md: "25%" }}
+        left='50%'
+        fontSize={{ base: "24px", md: "48px" }}
+        zIndex={3}
+        textAlign='center'
+      >
+        <Text filter='drop-shadow(1px 1px 4px white)' as='span'>
+          Hi, I'm Jason.
+        </Text>
+        <Text opacity='.7' maxW='700px' fontSize='20px'>
+          I'm an expert Web and Mobile{" "}
+          <Box as='span' bgColor='coral'>
             {text}
-          </Text>
+          </Box>
           <Cursor cursorBlinking cursorStyle='_' cursorColor='coral' />
-          <Text opacity='0' pointerEvents='none' lineHeight='0'>
-            Get to know me!
-          </Text>
-        </Box>
-      )}
-      {showActions && (
-        <MotionBox
-          zIndex={3}
-          display='flex'
-          pos='absolute'
-          bottom='25%'
-          transform='translate(-50%, 40px)'
-          left='50%'
-          gap='16px'
-          justify='center'
-          initial={{
-            opacity: 0,
+        </Text>
+      </MotionBox>
+
+      <MotionBox
+        pos='absolute'
+        bottom={{ base: "20%", md: "18%" }}
+        left='50%'
+        transform='translateX(-50%)'
+        zIndex={5}
+      >
+        <Button
+          p='16px 32px'
+          borderRadius='20px'
+          borderWidth='2px'
+          variant='outline'
+          borderColor='coral'
+          filter='drop-shadow(1px 1px 2px coral)'
+          color='white'
+          transition='all .4s'
+          _hover={{
+            borderColor: "white",
+            color: "coral",
           }}
-          animate={{
-            opacity: 1,
-          }}
-          transition={{
-            duration: 1,
-          }}
+          onClick={() => handleClick("Projects")}
         >
-          <Button
-            _hover={{
-              filter: "drop-shadow(2px 2px 8px cyan)",
-            }}
-            boxShadow='2px 2px 4px gray'
-            color='white'
-            bgColor='coral'
-            transition='all .4s'
-            onClick={() => handleClick("Contact")}
-          >
-            Contact
-          </Button>
-          <Button
-            _hover={{
-              filter: "drop-shadow(2px 2px 8px cyan)",
-            }}
-            boxShadow='2px 2px 4px gray'
-            variant='outline'
-            borderColor='coral'
-            transition='all .4s'
-            color='white'
-            onClick={() => handleClick("Projects")}
-          >
-            Projects
-          </Button>
-        </MotionBox>
-      )}
+          See my work below
+        </Button>
+      </MotionBox>
+
       <Box
         fontWeight='thin'
         pos='absolute'
@@ -135,7 +109,7 @@ function Hero() {
         bgRepeat='no-repeat'
         zIndex={2}
         bgImage={
-          "linear-gradient(to bottom, rgba(0,0,0,0), rgba(0,0,0,.4)), url('/assets/hero-me.png')"
+          "linear-gradient(to bottom, rgba(0,0,0,0), rgba(0,0,0,.6)), url('/assets/hero-me.png')"
         }
         borderRadius='full'
         initial={{
