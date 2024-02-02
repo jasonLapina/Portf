21a1dd5a167bd00365cfe2c98623a89efafcb6a0
Jason Lapina <jasonlapina.blink@gmail.com>
2024-02-02 15:23:12 +0800

    3d heading remove camera rig

--- src/components/3d/3DHeading.js
+++ src/components/3d/3DHeading.js
@@ -1,35 +1,18 @@
 import { useMediaQuery } from "@chakra-ui/react";
 import { Text3D, Center, Sparkles } from "@react-three/drei";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { easing } from "maath";
-import { Suspense, useRef } from "react";
-
-function CameraRig({ children }) {
-  const groupRef = useRef();
-
-  useFrame((state, delta) => {
-    easing.dampE(
-      groupRef.current.rotation,
-      [-state.pointer.y / 10, state.pointer.x / 25, 0],
-      0.25,
-      delta
-    );
-  });
-  return <group ref={groupRef}>{children}</group>;
-}
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
 
 function Copy({ text }) {
   const [isMd] = useMediaQuery("(min-width: 767px)");
 
   return (
-    <CameraRig>
-      <Center position={[isMd ? -5 : 0, 0, 1]}>
-        <Text3D size={isMd ? 2 : 1} font='/Heading.json'>
-          {text}
-          <meshNormalMaterial />
-        </Text3D>
-      </Center>
-    </CameraRig>
+    <Center position={[isMd ? -5 : 0, 0, 1]}>
+      <Text3D size={isMd ? 2 : 1} font='/Heading.json'>
+        {text}
+        <meshNormalMaterial />
+      </Text3D>
+    </Center>
   );
 }
 
